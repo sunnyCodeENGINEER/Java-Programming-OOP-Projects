c403e63a55429be41ffa26fff57de972b019512a
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2022-11-30 00:24:02 +0000

    Preparing directory for first actual Java project.

---
+++ Java Project 1/Pen Project/src/OOP_Project1.java
@@ -0,0 +1,7 @@
+public class OOP_Project1 {
+    public static void main(String[] args) {
+        // this project will be used by the user to store and keep track of all items in an inventory.
+
+        //TODO : Create classes for each kind of object that can be stored in the inventory.
+    }
+}
