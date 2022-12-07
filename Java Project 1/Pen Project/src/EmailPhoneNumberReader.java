7a8a8e54985a75477cdfe309aa61f9cd0f7bb93d
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2022-12-07 21:50:45 +0000

    Still working on project.

--- Java Project 1/Pen Project/src/EmailPhoneNumberReader.java
+++ Java Project 1/Pen Project/src/EmailPhoneNumberReader.java
@@ -3,12 +3,12 @@ import java.util.regex.*;
 
 public class EmailPhoneNumberReader {
     public static void main(String[] args) {
-        String results;
-        String emailRE = "\\S+@\\w+.\\w{2, 4}";
+        String results = "";
+        String emailRE = "\\S+@\\w+.\\w{2,4}";
         String phoneRE = "\\d{3}\\s\\d{3}\\s\\d{4}";
 
         Pattern emailPattern = Pattern.compile(emailRE);
-        Pattern PhonePattern = Pattern.compile(phoneRE);
+        Pattern phonePattern = Pattern.compile(phoneRE);
         // Matcher match1 = pattern1.matcher(text);
 
         try {
@@ -17,13 +17,27 @@ public class EmailPhoneNumberReader {
         String s;
         while((s = reader.readLine()) != null) {
             // bw2.write(s +"\n");
-            
+            Matcher emailMatcher = emailPattern.matcher(s);
+            Matcher phoneMatcher = phonePattern.matcher(s);
+
+            // if (emailMatcher.matches()) {
+                results += emailMatcher.group() + " === ";
+            // }
+
+            if (phoneMatcher.matches()) {
+                results += phoneMatcher.group();
+            }
             System.out.println(s);
+
+            
+            System.out.println(results);
         }
 
         reader.close();
         } catch(Exception ex) {
             System.out.println(ex.getLocalizedMessage());
         }
+
+        System.out.println(results);
     }
 }
