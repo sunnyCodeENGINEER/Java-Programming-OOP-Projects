07320734b137b85ff85d7584854dbca0ff3b99c6
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2022-12-09 23:24:17 +0000

    Still learning on Object Oriented Programming in Java.

---
+++ Java Project 1/Pen Project/src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Animal {
+    
+    public Dog (String name, String breed, int age) {
+        this.age = age;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    @Override
+    void makeSound() {
+        // TODO Auto-generated method stub
+        System.out.println("Woof-woof!!!");
+    }
+}
--- Java Project 1/Pen Project/src/JavaGenericsMain.java
+++ Java Project 1/Pen Project/src/JavaGenericsMain.java
@@ -2,6 +2,33 @@ public class JavaGenericsMain {
     public static void main(String[] args) {
         JavaGenerics<Integer> thingToPrint = new JavaGenerics<>(24);
 
+        Dog billy = new Dog("Billy", "Chihuahua", 3);
+
+        System.out.println(billy.age);
+        billy.grow();
+        System.out.println(billy.age);
+
         thingToPrint.printOut();
     }
+}
+
+abstract class Animal implements LivingThing {
+    protected int age;
+    protected String name;
+    protected String sex;
+    protected String breed;
+
+    @Override
+    public void grow() {
+        // TODO Auto-generated method stub
+        ++this.age; 
+    }
+
+    abstract void makeSound();
+
+}
+
+interface LivingThing {
+
+    public void grow(); 
 }
